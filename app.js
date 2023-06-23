d736c2516aafb47e34b1fb96380fd183a3ab2c60
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-23 14:10:59 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import fs from 'fs';
+
+const app = express();
+const port = 3000;
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
