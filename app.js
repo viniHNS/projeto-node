4f6e41b45bf41793a704911864056ab3591a5bc4
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-29 15:57:45 +0000

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { engine } from 'express-handlebars';
 import fs from 'fs';
 import bodyParser from 'body-parser';
 import pdfKit from 'pdfkit';
+import Swal from 'sweetalert2';
 
 const app = express();
 const port = 3000;
@@ -22,28 +23,32 @@ app.get('/', (req, res) => {
 });
 
 app.post('/gerarPDF', (req, res) => {
+    const email = req.body.email;
+    const name = req.body.nome;
+    
     const doc = new pdfKit();
 
-    doc.pipe(fs.createWriteStream('output.pdf'));
+    // Define o cabeçalho HTTP para o PDF
+    res.setHeader('Content-Disposition', 'attachment; filename="example.pdf"');
+    res.setHeader('Content-Type', 'application/pdf');
 
-    doc.text(req.body.nome);
+   // Pipe o PDF para a resposta
+    doc.pipe(res);
 
-    
+  // Adiciona o conteúdo do formulário ao PDF
+    doc.font('Helvetica');
+    doc.fontSize(24).text('Informações do Formulário:', { align: 'center' });
+    doc.fontSize(14).text(`Nome: ${name}`);
+    doc.fontSize(14).text(`E-mail: ${email}`);
 
-    const stream = doc.pipe(blobStream());
-
-
+  // Finaliza o PDF e encerra a resposta
     doc.end();
-    stream.on('finish', function() {
-        const blob = stream.toBlob('application/pdf');
 
-  
-        const url = stream.toBlobURL('application/pdf');
-        iframe.src = url;
-    });
-
-
-
+    Swal.fire(
+        'The Internet?',
+        'That thing is still around?',
+        'success'
+    )
 
 });
 
