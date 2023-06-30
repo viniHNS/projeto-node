6263e61fb79fe51101f78c9d5bcb57ccdfcafaf1
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-06-30 14:26:46 +0000

    adicionada verificacao dos campos

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
 VMasker(document.querySelector("#telefone")).maskPattern("(99) 9999-9999");
+
+let isNomeSet = false;
+let isEmailSet = false;
+let clicked = false;
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+
+
