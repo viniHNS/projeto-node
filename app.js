5e4f285df3e6e02702f7008113a1013cfdc6efa8
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-07-13 17:36:25 +0000

    corrigido erro

--- app.js
+++ app.js
@@ -463,10 +463,11 @@ app.get('/controlaUsuario', checkToken, isAdmin, async (req, res) => {
   const usuario = require('./models/User.js');
   const usuarioAtualID = req.userId;
   try {
+    
     let usuarios = await usuario.find({ _id: { $ne: usuarioAtualID } })
-      .select('-senha -__v -createdAt -updatedAt')
-      .lean();
-    res.render('controlaUsuario', { layout: 'admin' })
+    .select('-senha -__v -createdAt -updatedAt')
+    .lean();
+    res.render('controlaUsuario', { layout: 'admin', usuarios: usuarios})
   } catch (error) {
     console.error('Erro ao buscar dados:', error);
     res.status(500).redirect('https://http.cat/images/500.jpg');
