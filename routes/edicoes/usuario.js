a84acc621c4897b915f990edcff839e7e536d853
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-07-18 13:09:15 +0000

    removido helper

--- routes/edicoes/usuario.js
+++ routes/edicoes/usuario.js
@@ -9,35 +9,22 @@ const mongoose = require('mongoose');
 
 conn();
 
-function isInTurmasSelecionadas(turmaId, turmasSelecionadas) {
-  return turmasSelecionadas.includes(turmaId);
-}
-
-const handlebars = require('handlebars');
-handlebars.registerHelper('isInTurmasSelecionadas', isInTurmasSelecionadas);
-
 router.get('/editarUsuario/:id', async (req, res) => {
   const turma = require('../../models/Turma.js');
   const usuario = require('../../models/User.js');
   let id = req.params.id;
-  let turmasPermitidas;
+  const dados = await usuario.findById(id).lean();
+
+
   try {
-    const dados = await usuario.findById(id).lean();
     const dadosTurma = await turma.find().lean();
-
-    const documentos = await usuario.find({ 'turmasPermitidas.0': { $exists: true } }).lean();
-
-    if (documentos.length > 0) {
-      documentos.forEach((documento) => {
-        turmasPermitidas = documento.turmasPermitidas;
-        console.table(turmasPermitidas);
+    res.render('editar/editarUsuario', {
+      layout: 'admin',
+      dados: dados,
+      turmas: dadosTurma
     });
-  } else {
-    console.log('Nenhum documento encontrado');
   }
-
-    res.render('editar/editarUsuario', { layout: 'admin', dados: dados, dadosTurma: dadosTurma, turmasPermitidas: turmasPermitidas});
-  } catch (error) {
+    catch (error) {
     console.error('Erro ao buscar dados:', error);
     res.status(500).redirect('https://http.cat/images/500.jpg');
   }
@@ -48,6 +35,13 @@ router.post('/editarUsuario/:id', async (req, res) => {
   const turma = require('../../models/Turma.js');
 
   let { nome, email, senha, ativo } = req.body;
+  let dadosCorpo = req.body;
+
+  const turmasSelecionadas = Object.keys(dadosCorpo)
+    .filter(key => key.startsWith('turma-'))
+    .map(key => key.split('-')[1]);
+
+  
   const id = req.params.id;
   const dados = await usuario.findById(id).lean();
   let senhaAntiga = dados.senha;
@@ -70,14 +64,6 @@ router.post('/editarUsuario/:id', async (req, res) => {
     return res.status(422).render('editar/editarUsuario', { layout: 'admin', errorEmailVazio: 'Email não informado' });
   }
 
-  const turmasSelecionadas = Object.keys(req.body)
-  .filter(key => key.startsWith('turma-'))
-  .map(key => {
-    const turmaId = key.split('-')[1]; // Obtém o ID da turma do nome do campo
-    return { id: turmaId };
-  });
-  
-  console.table(turmasSelecionadas)
   if(nome || email || senha) {
     try {
       const salt = await bcrypt.genSalt(10);
@@ -95,11 +81,8 @@ router.post('/editarUsuario/:id', async (req, res) => {
       if(senhaNova == senhaAntiga){
         senhaNova = senhaAntiga;
       }
-      const dados = await usuario.findByIdAndUpdate(id, { nome: nome, email: email, senha: senhaNova, ativo: isAtivo });
 
-      const dadosTurmas = await usuario.findById(id);
-      dadosTurmas.turmasPermitidas.push(...turmasSelecionadas);
-      await dadosTurmas.save();
+      await usuario.findByIdAndUpdate(id, { nome: nome, email: email, senha: senhaNova, ativo: isAtivo, turmasPermitidas: turmasSelecionadas });
 
       res.redirect('/controlaUsuario');
     } catch (error) {
