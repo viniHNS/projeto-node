24fc121d952011ed9d8b4ac546886d7bf8cdaa6c
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-07-25 14:15:40 +0000

    consulta de turmas em que o prof esta vinculado

--- routes/consultas/turmaChamada.js
+++ routes/consultas/turmaChamada.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const conn = require('../../db/conn');
-
-conn();
+const turma = require('../../models/Turma.js');
+const aluno = require('../../models/Aluno.js');
+const user = require('../../models/User.js');
 
 router.get('/consultaTurmaChamada', async (req, res) => {
-    const turma = require('../../models/Turma.js');
-    const aluno = require('../../models/Aluno.js');
-    const professor = require('../../models/User.js')
-  
-    try {
-      let turmas = await turma.find().sort({periodo: 1}).lean();
-  
-      for (let i = 0; i < turmas.length; i++) {
-        let turmaId = turmas[i]._id;
-        let quantidadeAlunos = await aluno.countDocuments({ 'turma.id': turmaId });
-        turmas[i].quantidadeAlunos = quantidadeAlunos;
-  
-      }
-    
-      res.render('consulta/consultaTurmaChamada', { layout: 'admin', turmas: turmas });
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-      res.status(500).redirect('https://http.cat/images/500.jpg');
+  try {
+    const professoresVinculados = req.userId; 
+
+    // Consulta apenas as turmas em que o professor está vinculado
+    const turmas = await turma.find({ professoresVinculados: professoresVinculados }).sort({ periodo: 1 }).lean();
+
+    for (let i = 0; i < turmas.length; i++) {
+      const turmaId = turmas[i]._id;
+      const quantidadeAlunos = await aluno.countDocuments({ 'turma.id': turmaId });
+      turmas[i].quantidadeAlunos = quantidadeAlunos;
     }
+
+    res.render('consulta/consultaTurmaChamada', { layout: 'admin', turmas: turmas });
+  } catch (error) {
+    console.error('Erro ao buscar dados:', error);
+    res.status(500).redirect('https://http.cat/images/500.jpg');
+  }
 });
-  
-  module.exports = router;
+
+module.exports = router;
