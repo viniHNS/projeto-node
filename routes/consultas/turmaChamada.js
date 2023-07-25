8afcf362bcc4c249387c89cddb495bfc6ec94f8d
Vinicius Hissayoshi Nishida <88986579+viniHNS@users.noreply.github.com>
2023-07-25 13:52:40 +0000

    commit rushado

---
+++ routes/consultas/turmaChamada.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const conn = require('../../db/conn');
+
+conn();
+
+router.get('/consultaTurmaChamada', async (req, res) => {
+    const turma = require('../../models/Turma.js');
+    const aluno = require('../../models/Aluno.js');
+    const professor = require('../../models/User.js')
+  
+    try {
+      let turmas = await turma.find().sort({periodo: 1}).lean();
+  
+      for (let i = 0; i < turmas.length; i++) {
+        let turmaId = turmas[i]._id;
+        let quantidadeAlunos = await aluno.countDocuments({ 'turma.id': turmaId });
+        turmas[i].quantidadeAlunos = quantidadeAlunos;
+  
+      }
+    
+      res.render('consulta/consultaTurmaChamada', { layout: 'admin', turmas: turmas });
+    } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+      res.status(500).redirect('https://http.cat/images/500.jpg');
+    }
+});
+  
+  module.exports = router;
